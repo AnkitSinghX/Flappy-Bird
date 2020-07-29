721175182e411d30cb1ef43c422538f0c4469496
AnkitSinghX <ankit123418@gmail.com>
2020-07-29 20:47:42 +0530

    Adding all my files

---
+++ app/src/main/java/com/ankit/flappy_bird/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ankit.flappy_bird;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
